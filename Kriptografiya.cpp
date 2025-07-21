547c06daea5af0da0e99236a24e3f3ee1bf76668
Roky31 <rokysupraaa@gmail.com>
2025-07-21 21:44:07 +0300

    Update Kriptografiya.cpp

--- Kriptografiya.cpp
+++ Kriptografiya.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-// Harfi kaydiran fonksiyon
+
 char shiftChar(char c, int shift) {
     if (c >= 'A' && c <= 'Z') {
-        // Büyük harf ise
+        
         return (c - 'A' + shift) % 26 + 'A';
     } else if (c >= 'a' && c <= 'z') {
-        // Küçük harf ise
+        
         return (c - 'a' + shift) % 26 + 'a';
     } else {
-        // Harf degilse degistirme
+        
         return c;
     }
 }
 
-// Mesaji sifreleyen fonksiyon
+
 string encrypt(string text, int shift) {
     string result = "";
     for (char c : text) {
@@ -26,9 +26,9 @@ string encrypt(string text, int shift) {
     return result;
 }
 
-// Mesaji çözen fonksiyon
+
 string decrypt(string text, int shift) {
-    // Sifreyi çözerken kaydirmayi ters yapiyoruz
+    
     return encrypt(text, 26 - shift);
 }
 
@@ -36,17 +36,17 @@ int main() {
     string message;
     int shift;
 
-    cout << "Bir mesaj gir: ";
+    cout << "Write massage: ";
     getline(cin, message);
 
-    cout << "Kaydirma miktarini gir (1-25): ";
+    cout << "Choose one number (1-25): ";
     cin >> shift;
 
     string encrypted = encrypt(message, shift);
-    cout << "Sifrelenmis mesaj: " << encrypted << endl;
+    cout << "Encrypted: " << encrypted << endl;
 
     string decrypted = decrypt(encrypted, shift);
-    cout << "Cozulmus mesaj: " << decrypted << endl;
+    cout << "Decrypted: " << decrypted << endl;
 
     return 0;
 }
